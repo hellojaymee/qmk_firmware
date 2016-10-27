2707b07d3f6ef2deb6a32849b0cec0467ab28313
Jaymee Robertson <jaymee.robertson@gmail.com>
2016-10-27 09:35:57 -0500

    add tilde macro

--- keyboards/kc60/keymaps/jaymee/keymap.c
+++ keyboards/kc60/keymaps/jaymee/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * Holding capslock key gives a layer like the KBParadise v60.
    */
   [0] = KEYMAP( /* Basic QWERTY */
-      KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,    KC_8,  KC_9,     KC_0,     KC_MINS,  KC_EQL,    KC_BSPC,  \
+      M(0),   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,    KC_8,  KC_9,     KC_0,     KC_MINS,  KC_EQL,    KC_BSPC,  \
       KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,  KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,  \
       MO(1),    KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,    KC_K,  KC_L,     KC_SCLN,  KC_QUOT,  __NO___,   KC_ENT,   \
       KC_LSFT,  __NO___,  KC_Z,     KC_X,    KC_C,     KC_V,     KC_B,     KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  __NO___,   KC_RSFT,  \
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = KEYMAP( /* KBP v60-like arrows, media keys, etc */
       KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,    KC_DEL,   \
       _______,  _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  BL_INC,    BL_DEC,    BL_STEP,  \
-      _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______,  _______,  KC_VOLD,  KC_MUTE,  KC_VOLU,  _______,  _______,   __NO___,   M(0),     \
+      _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______,  _______,  KC_VOLD,  KC_MUTE,  KC_VOLU,  _______,  _______,   __NO___,   M(1),     \
       _______,  __NO___,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   __NO___,   _______,  \
       _______,  _______,  _______,                      KC_SPC,                                           DEBUG,    RESET,     _______,   __NO___   \
       ),
@@ -31,14 +31,27 @@ const uint16_t PROGMEM fn_actions[] = {
 
 };
 
+
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   switch(id) {
-     case 0:
-     if (record->event.pressed) {
-       return MACRO( I(5), D(LSFT), T(SCLN), U(LSFT), T(W), T(Q), T(ENT), END  );
-     }
-     break;
-   }
+    case 0: {
+      static uint8_t code;
+      if (record->event.pressed) {
+        code = (keyboard_report->mods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT))) ? KC_GRV : KC_ESC;
+        register_code(code);
+      }
+      else {
+        unregister_code(code);
+      }
+      break;
+    }
+    break;
+    case 1:
+    if (record->event.pressed) {
+      return MACRO( I(5), D(LSFT), T(SCLN), U(LSFT), T(W), T(Q), T(ENT), END  );
+    }
+    break;
+  }
   return MACRO_NONE;
 };
